82c7e37333d9b537095b8dd8e54622a0b9c77431
Tommy Dang <nhontuan@gmail.com>
2017-01-25 16:29:46 -0600

    Update project page

--- researchProjects.php
+++ researchProjects.php
@@ -20,7 +20,7 @@ include 'menu.php'; ?>
    <div style="margin-top: 5px">
     <left> Related publications: <br>
     <ul>
-    <li> <b>Tuan Dang</b>, Leland Wilkinson, and Anushka Anand. <i>Stacking Graphic Elements to Avoid Over-Plotting</i>. IEEE Transactions on Visualization AndComputer Graphics 16(6), InfoVis 2010.  <a href="file/Stacking.pdf"> <img src="img/pdf.png" height="16"></a> <a href="https://www.cs.uic.edu/~tdang/media/Stacking.mp4"> <img src="img/movie.png" height="16"></a> <a href="BibTeX/Dang2010Stacking.txt"> <img src="img/bibtex.png" height="16"></a> 
+    <li> <b>Tuan Dang</b>, Leland Wilkinson, and Anushka Anand. <i>Stacking Graphic Elements to Avoid Over-Plotting</i>. IEEE Transactions on Visualization AndComputer Graphics 16(6), InfoVis 2010.  <a href="file/Stacking.pdf"> <img src="images/icons2/pdf.png" height="16"></a> <a href="https://www.cs.uic.edu/~tdang/media/Stacking.mp4"> <img src="images/icons2/movie.png" height="16"></a> <a href="BibTeX/Dang2010Stacking.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
 <a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=5613442" class="button">DOI</a>
 </li>
     </ul>
@@ -51,10 +51,10 @@ or brightness could reveal the threefold increase in Lyme disease cases centered
         <li><b>Tommy Dang</b>, Paul Murray, Ronak Etemadpour, and Angus Forbes. <i> Visualizing Structure and Connectivity in Complex Hierarchical Datasets</i>. IEEE Transactions on Visualization and Computer Graphics. (<i>in submission</i>)</li>
         <li> <b>Tommy Dang</b> and Angus Forbes. <i> CactusTree: A Tree Drawing Approach for Hierarchical Edge Bundling</i>. Proceedings of the 10th IEEE Pacific Visualization Symposium, PacificVis 2017. </li>         
         <li><b>Tommy Dang</b>,  Paul Murray, Jillian Aurisano, and Angus Forbes. <i> ReactionFlow: An Interactive Visualization Tool for Causality Analysis in Biological Pathways</i>. BMC Proceedings 9(6), BioVis 2015. 
-          <a href="ReactionFlow/ReactionFlow_BioVis_2015.pdf"> <img src="img/pdf.png" height="16"></a>
-          <a href="ReactionFlow/video.mp4"> <img src="img/movie.png" height="16"></a>
-          <a href="https://github.com/CreativeCodingLab/ReactionFlow"> <img src="img/github2.png" height="16"></a>
-          <a href="BibTeX/Dang2015ReactionFlow.txt"> <img src="img/bibtex.png" height="16"></a> 
+          <a href="ReactionFlow/ReactionFlow_BioVis_2015.pdf"> <img src="images/icons2/pdf.png" height="16"></a>
+          <a href="ReactionFlow/video.mp4"> <img src="images/icons2/movie.png" height="16"></a>
+          <a href="https://github.com/CreativeCodingLab/ReactionFlow"> <img src="images/icons2/github.png" height="16"></a>
+          <a href="BibTeX/Dang2015ReactionFlow.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
           <a href="http://www.ncbi.nlm.nih.gov/pubmed/26361502" class="button">DOI</a>
           </li>
         
@@ -64,10 +64,10 @@ or brightness could reveal the threefold increase in Lyme disease cases centered
           
 
     <div  align="middle" style="margin-top: -35px"> <img src="images/projectFigures/HCL.png"  width=100%></div>
-  <left><b>Visualizing hierarchical structures and prey-predator relationships </b> of mammals (left) and birds (right). Each animal is presented as a green leaf node. Red links are prey-predator relationships between them.</left>
+  <left>Visualizing hierarchical structures and prey-predator relationships of mammals (left) and birds (right). Each animal is presented as a green leaf node. Red links are prey-predator relationships between them.</left>
   
   <div  align="middle" style="margin-top: 20px"> <img src="images/projectFigures/HCLdiseases.png"  width=90%></div>
-  <left><b>Visualizing causal relationships</b> of 20 popular diseases (left) from <a href="http://www.reactome.org/">reactome</a> and HIV life cycle (right): Gradient links indicate causalities between biochemical reactions.</left>
+  <left>Visualizing causal relationships of 20 popular diseases (left) from <a href="http://www.reactome.org/">reactome</a> and HIV life cycle (right): Gradient links indicate causalities between biochemical reactions.</left>
   
     <div id="site_content"></div> 
     <div id="top_border"></div> 
@@ -78,27 +78,59 @@ or brightness could reveal the threefold increase in Lyme disease cases centered
     <left> Related publications: <br>
     <ul>
         <li><b>Tommy Dang</b>,  Nick Pendar, and Angus Forbes. <i> TimeArcs: Visualizing Fluctuations in Dynamic Networks</i>. Computer Graphics Forum 35(3), EuroVis 2016. 
-        <a href="https://www.cs.uic.edu/~tdang/TimeArcs/EuroVis2016/TimeArcs_Dang_EuroVis2016.pdf"> <img src="img/pdf.png" height="16"></a>
-        <a href="https://www.cs.uic.edu/~tdang/TimeArcs/EuroVis2016/TimeArcs_Dang_EuroVis2016.mp4"> <img src="img/movie.png" height="16"></a>
-        <a href="https://github.com/CreativeCodingLab/TimeArcs"> <img src="img/github2.png" height="16"></a>
-        <a href="BibTeX/Dang2016TimeArcs.txt"> <img src="img/bibtex.png" height="16"></a> 
+        <a href="https://www.cs.uic.edu/~tdang/TimeArcs/EuroVis2016/TimeArcs_Dang_EuroVis2016.pdf"> <img src="images/icons2/pdf.png" height="16"></a>
+        <a href="https://www.cs.uic.edu/~tdang/TimeArcs/EuroVis2016/TimeArcs_Dang_EuroVis2016.mp4"> <img src="images/icons2/movie.png" height="16"></a>
+        <a href="https://github.com/CreativeCodingLab/TimeArcs"> <img src="images/icons2/github.png" height="16"></a>
+        <a href="BibTeX/Dang2016TimeArcs.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
         <a href="http://diglib.eg.org/handle/10.1111/cgf12882" class="button">DOI</a>
-        </li>
-        
+        </li>   
     </ul>
     </left>
    </div>    
 
     <div align="middle" style="margin-top: -30px"> <img src="images/projectFigures/PoliticalBlogs.png"  width=100%></div>
-  <center>Major events from political blogs were automatically detected in my visualization.</center>
+  <left>Major events from nearly 100,000 political blogs in last 10 years were automatically detected in our visualization: Links connect popular terms appeared in the same blogs.</left>
   
 
-  <hr style="width: 100%; height: 0.2px;" color="black">
-    <div align="left"><font style="font-family:
+  <div id="site_content"></div> 
+    <div id="top_border"></div> 
+      <div align="left"><font style="font-family:
         Helvetica,Arial,sans-serif;" color="black" size="4">Matrix visualization</font></div>
-    <div  align="middle"> <img src="images/projectFigures/Matrix1.png"  width=100%></div>
-  <center>Visualizing the alignment of <a href="http://taxonbytes.org/pathwaymatrix-visualization-software-shows-eulerx-taxonomy-alignment-products-and-ambiguities/"> taxonomic classifications</a>: Gymnospermae sec. Weakley (2010) vs. RAB (1968)</center>
+  <div style="margin-top: 5px">
+    <left> Related publications: <br>
+    <ul>
+        <li> <b>Tommy Dang</b>, Hong Cui, and Angus Forbes. <i> MultiLayerMatrix: Visualizing Large Taxonomic Datasets</i>. The seventh international EuroVis workshop on Visual Analytics, EuroVA 2016.  
+          <a href="https://www.cs.uic.edu/~tdang/MultiLayerMatrix/MultiLayerMatrix_EuroVA_2016.pdf"> <img src="images/icons2/pdf.png" height="16"></a>
+          <a href="MultiLayerMatrix/video.mp4"> <img src="images/icons2/movie.png" height="16"></a>
+          <a href="https://github.com/CreativeCodingLab/MultiLayerMatrix"> <img src="images/icons2/github.png" height="16"></a>
+          <a href="BibTeX/Dang2016MultiLayerMatrix.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
+          <a href="https://diglib.eg.org/handle/10.2312/eurova20161125" class="button">DOI</a>
+        </li>
+        <li> <b>Tommy Dang</b>, Nico Franz, Bertram Lud&#228;scher and Angus Forbes. <i> ProvenanceMatrix: A Visualization Tool for Multi-Taxonomy Alignments</i>. International Workshop on Visualizations and User Interfaces for Ontologies and Linked Data, VOILA 2015. 
+          <a href="ProvenanceMatrix/ProvenanceMatrix_VOILA_2015.pdf"> <img src="images/icons2/pdf.png" height="16"></a>
+          <a href="ProvenanceMatrix/video.mp4"> <img src="images/icons2/movie.png" height="16"></a>
+          <a href="https://github.com/CreativeCodingLab/ProvenanceMatrix"> <img src="images/icons2/github.png" height="16"></a>
+          <a href="BibTeX/Dang2015ProvenanceMatrix.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
+          <a href="http://www.bibsonomy.org/bibtex/23a2e7eda5e8580c2bdfdefca6d152a0f/dblp" class="button">DOI</a>
+        </li> 
+        <li> <b>Tommy Dang</b>,  Paul Murray, and Angus Forbes. <i> PathwayMatrix: Visualizing Binary Relationships between Proteins in Biological Pathways</i>. BMC Proceedings 9(6), BioVis 2015. 
+          <a href="PathwayMatrix/PathwayMatrix_BioVis_2015.pdf"> <img src="images/icons2/pdf.png" height="16"></a>
+          <a href="PathwayMatrix/PathwayMatrixBioVis960x540.mp4"> <img src="images/icons2/movie.png" height="16"></a>
+          <a href="https://github.com/CreativeCodingLab/PathwayMatrix"> <img src="images/icons2/github.png" height="16"></a>
+          <a href="BibTeX/Dang2015PathwayMatrix.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
+          <a href="http://www.ncbi.nlm.nih.gov/pubmed/26361499" class="button">DOI</a>
+        </li>
+        <li> <b>Tommy Dang</b>, Anushka Anand, and Leland Wilkinson. <i>TimeSeer: Scagnostics for High-Dimensional Time Series</i>. IEEE Transactions on Visualization and Computer Graphics 19(3), TVCG 2012.  
+          <a href="file/Multi-Scagnostics.pdf"> <img src="images/icons2/pdf.png" height="16"></a>  
+          <a href="media/TimeSeer.mov"> <img src="images/icons2/movie.png" height="16"></a>
+          <a href="BibTeX/Dang2012Timeseer.txt"> <img src="images/icons2/bibtex.png" height="16"></a> 
+          <a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=6200267" class="button">DOI</a>
+        </li>
+    </ul>
+    </left>
+   </div>  
+         
+    <div  align="middle" style="margin-top: -30px"> <img src="images/projectFigures/Matrix1.png"  width=100%></div>
+  <left>Visualizing the alignment of <a href="http://taxonbytes.org/pathwaymatrix-visualization-software-shows-eulerx-taxonomy-alignment-products-and-ambiguities/"> taxonomic classifications</a>: Gymnospermae sec. Weakley (2010) vs. RAB (1968)</left>
       </div>
-
-
 <?php include 'footer.php'; ?>
