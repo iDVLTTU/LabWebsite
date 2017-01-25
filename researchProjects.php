1a409b8a9de7ce04aae95888b7bfc49dbec2ee49
Tommy Dang <nhontuan@gmail.com>
2017-01-25 12:28:07 -0600

    Update projects

--- researchProjects.php
+++ researchProjects.php
@@ -5,36 +5,47 @@ include 'menu.php'; ?>
 
 
 <div id="site_content">
-	  <hr style="width: 100%; height: 0.2px;" color="black">
+	 <div id="top_border"></div> 
+
     <div align="left"><font style="font-family:
-        Helvetica,Arial,sans-serif;" color="black" size="4">Geo-spatial visualizations</font></div>
-        
-  <div  align="middle" style="margin-top: 10px"> <img src="images/projectFigures/UFO.png"  width=94%></div>
-  <center>Visualizing UFO sightings on a 2D map</center>
-  <div  align="middle" style="margin-top: 30px"> <img src="images/projectFigures/lyme.png"  width=92%></div>
+        Helvetica,Arial,sans-serif;" color="black" size="4">Geospatial-temporal visualizations</font></div>
+   <div style="margin-top: 5px">
+    <left> Related publication: <br>
+    <ul>
+    <li> <b>Tuan Dang</b>, Leland Wilkinson, and Anushka Anand. <i>Stacking Graphic Elements to Avoid Over-Plotting</i>. IEEE Transactions on Visualization AndComputer Graphics 16(6), InfoVis 2010.  <a href="file/Stacking.pdf"> <img src="img/pdf.png" height="16"></a> <a href="https://www.cs.uic.edu/~tdang/media/Stacking.mp4"> <img src="img/movie.png" height="16"></a> <a href="BibTeX/Dang2010Stacking.txt"> <img src="img/bibtex.png" height="16"></a> 
+<a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=5613442" class="button">DOI</a>
+</li>
+    </ul>
+    </left>
+  </div>
+
+  <div  align="middle" style="margin-top: -20px"> <img src="images/projectFigures/UFO2.png"  width=100%></div>
+  <left> <b>Visualizing UFO sightings on a 2D map:</b> This project uses data from the National UFO Reporting Center available <a href="http://www.nuforc.org/webreports/ndxloc.html">here</a>. 
+The goal here is to give the user a higher level view of where different types of sightings occur, and to detect patterns/trends of these sightings over time.</left>
+  <div  align="middle" style="margin-top: 10px"> <img src="images/projectFigures/lyme.png"  width=75%></div>
   <center>Visualizing Lyme diseases in 3D</center>
      
 
   <hr style="width: 100%; height: 0.2px;" color="black">
     <div align="left"><font style="font-family:
         Helvetica,Arial,sans-serif;" color="black" size="4">Networks and structures</font></div>
-    <div  align="middle"> <img src="images/projectFigures/HCL.png"  width=99%></div>
+    <div  align="middle"> <img src="images/projectFigures/HCL.png"  width=100%></div>
   <center>Hierarchical structures and prey-predator relationships of mammals (left) and birds (right)</center>
   
-  <div  align="middle" style="margin-top: 30px"> <img src="images/projectFigures/HCLdiseases.png"  width=80%></div>
+  <div  align="middle" style="margin-top: 30px"> <img src="images/projectFigures/HCLdiseases.png"  width=70%></div>
   <center>Causal relationships of 20 diseases</center>
   
   <hr style="width: 100%; height: 0.2px;" color="black">
     <div align="left"><font style="font-family:
         Helvetica,Arial,sans-serif;" color="black" size="4">Dynamic network visualization</font></div>
-    <div  align="middle"> <img src="images/projectFigures/PoliticalBlogs.png"  width=88%></div>
+    <div  align="middle"> <img src="images/projectFigures/PoliticalBlogs.png"  width=100%></div>
   <center>Major events from political blogs were automatically detected in my visualization.</center>
   
 
   <hr style="width: 100%; height: 0.2px;" color="black">
     <div align="left"><font style="font-family:
         Helvetica,Arial,sans-serif;" color="black" size="4">Matrix visualization</font></div>
-    <div  align="middle"> <img src="images/projectFigures/Matrix.png"  width=81%></div>
+    <div  align="middle"> <img src="images/projectFigures/Matrix1.png"  width=100%></div>
   <center>Visualizing the alignment of <a href="http://taxonbytes.org/pathwaymatrix-visualization-software-shows-eulerx-taxonomy-alignment-products-and-ambiguities/"> taxonomic classifications</a>: Gymnospermae sec. Weakley (2010) vs. RAB (1968)</center>
       </div>
 
