ee2b61893bcbe889a3a42e4430825d63350e00a3
Tommy Dang <nhontuan@gmail.com>
2017-01-25 14:35:48 -0600

    Update projects

--- researchProjects.php
+++ researchProjects.php
@@ -3,14 +3,22 @@ $activePage = 'researchProjects.php';
 include 'menu.php'; ?>
 
 
-
 <div id="site_content">
+   <div id="top_border"></div> 
+   The works on this page were supported by:.<br>
+  <li style="margin-left: 20px"> 1) NSF DMS/<a href="https://www.nsf.gov/funding/pgm_summ.jsp?pims_id=501081">FODAVA</a> Grant  <a href="https://www.nsf.gov/cise/funding/2008_FODAVA_awards.xls"> No. 0808860</a>:
+   <i>Visually-Motivated Characterizations of Point Sets Embedded in High-Dimensional Geometric Spaces</i>  <br>
+</li><br>
+  <li style="margin-left: 20px"> 2) DARPA Big Mechanism Program under ARO contract <a href="https://www.evl.uic.edu/entry.php?id=2043"> WF911NF-14-1-0395</a>: <i>Reading and Assembling Contextual and Holistic Big Mechanisms</i> <br>
+</li><br>
+   
+<div id="site_content"></div> 
 	 <div id="top_border"></div> 
 
     <div align="left"><font style="font-family:
         Helvetica,Arial,sans-serif;" color="black" size="4">Geospatial-temporal visualizations</font></div>
    <div style="margin-top: 5px">
-    <left> Related publication: <br>
+    <left> Related publications: <br>
     <ul>
     <li> <b>Tuan Dang</b>, Leland Wilkinson, and Anushka Anand. <i>Stacking Graphic Elements to Avoid Over-Plotting</i>. IEEE Transactions on Visualization AndComputer Graphics 16(6), InfoVis 2010.  <a href="file/Stacking.pdf"> <img src="img/pdf.png" height="16"></a> <a href="https://www.cs.uic.edu/~tdang/media/Stacking.mp4"> <img src="img/movie.png" height="16"></a> <a href="BibTeX/Dang2010Stacking.txt"> <img src="img/bibtex.png" height="16"></a> 
 <a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=5613442" class="button">DOI</a>
@@ -19,16 +27,35 @@ include 'menu.php'; ?>
     </left>
   </div>
 
+ 
   <div  align="middle" style="margin-top: -20px"> <img src="images/projectFigures/UFO2.png"  width=100%></div>
   <left> <b>Visualizing UFO sightings on a 2D map:</b> This project uses data from the National UFO Reporting Center available <a href="http://www.nuforc.org/webreports/ndxloc.html">here</a>. 
 The goal here is to give the user a higher level view of where different types of sightings occur, and to detect patterns/trends of these sightings over time.</left>
   <div  align="middle" style="margin-top: 10px"> <img src="images/projectFigures/lyme.png"  width=75%></div>
-  <center>Visualizing Lyme diseases in 3D</center>
+  <left>Visualizing the distribution of Lyme diseases in 3D, reported in
+the U.S. in 2005, 2006, and 2007 by the <a href="https://www.cdc.gov/">CDC</a>. One might argue that a county-level choropleth map would
+be a suitable presentation. Nevertheless, it would be hard to argue that a county-level choropleth map using hue
+or brightness could reveal the threefold increase in Lyme disease cases centered around New England vs. the Midwest.
+</left>
      
 
-  <hr style="width: 100%; height: 0.2px;" color="black">
+  <div id="site_content"></div> 
+   <div id="top_border"></div> 
+
     <div align="left"><font style="font-family:
         Helvetica,Arial,sans-serif;" color="black" size="4">Networks and structures</font></div>
+
+    <div style="margin-top: 5px">
+    <left> Related publications: <br>
+    <ul>
+    <li> <b>Tuan Dang</b>, Leland Wilkinson, and Anushka Anand. <i>Stacking Graphic Elements to Avoid Over-Plotting</i>. IEEE Transactions on Visualization AndComputer Graphics 16(6), InfoVis 2010.  <a href="file/Stacking.pdf"> <img src="img/pdf.png" height="16"></a> <a href="https://www.cs.uic.edu/~tdang/media/Stacking.mp4"> <img src="img/movie.png" height="16"></a> <a href="BibTeX/Dang2010Stacking.txt"> <img src="img/bibtex.png" height="16"></a> 
+<a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=5613442" class="button">DOI</a>
+</li>
+    </ul>
+    </left>
+  </div>
+      
+
     <div  align="middle"> <img src="images/projectFigures/HCL.png"  width=100%></div>
   <center>Hierarchical structures and prey-predator relationships of mammals (left) and birds (right)</center>
   
