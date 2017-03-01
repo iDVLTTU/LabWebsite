4c7fb50c59bcea24bf2ddf9455b29399cdb49c28
Md. Yasin Kabir <mykabir0@gmail.com>
2017-03-01 11:31:15 -0600

    grants

--- grants.php
+++ grants.php
@@ -2,7 +2,7 @@
 $activePage = 'grants.php';
 include 'menu.php'; ?>
 
-
+<script data-require="jquery@*" data-semver="2.0.3" src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 
 <div id="site_content" class="conferences">
 	  <div id="top_border"></div>
@@ -16,33 +16,56 @@ include 'menu.php'; ?>
       <td> <b>Sponsor</td>
       <td> <b>Amount</td>
       <td> <b>Applicant Type</b> </td>
+      <td> <b>Citizenship or Residency</b> </td>
       <td> <b>Abstract</b></td>
       <td> <b>Deadline</b></td>
     </tr>
 
+     <tr bgcolor="#bbd">
+      <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/68670">Advances in Biological Informatics</a></td>
+      <td align="left" style="padding:5px;"><a href="">NSF</a></td>
+      <td align="left" style="padding:5px;">Unspecified</td>
+      <td align="left" style="padding:5px;">Academic Institution <br/> New Faculty/New Investigator</td>
+      <td align="left" style="padding:5px;">United States</td>
+      <td align="left" style="padding:5px;" title="The Advances in Biological Informatics (ABI) program seeks to encourage new approaches to the analysis and dissemination of biological knowledge for the benefit of both the scientific community and the broader public. The ABI program is especially interested in the development of informatics tools and resources that have the potential to advance- or transform- research in biology supported by the Directorate for Biological Sciences at the National Science Foundation.  The ABI program accepts three major types of proposals: Innovation awards that seek to pioneer new approaches to the application of informatics to biological problems, Development awards that seek to provide robust cyberinfrastructure that will enable transformative biological research, and Sustaining awards that seek to support ongoing operations and maintenance of existing cyberinfrastructure that is critical for continued advancement of priority biological research.">Hover to see abstract</td>
+      <td align="left" style="padding:5px;">09 Sep 2017</td>
+    </tr>
     
-    
-    <tr bgcolor="#bbd">
-      <td align="left" style="padding:5px;"><a href=""></a></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;">
-        <div class="tooltip">
-            <span class="tooltiptext"></span>
-        </div>
+    <tr bgcolor="#bdb">
+      <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/159865">Harnessing Big Data to Halt HIV</a></td>
+      <td align="left" style="padding:5px;"><a href="https://grants.nih.gov/grants/guide/pa-files/PA-15-273.html">HHS,NIH</a></td>
+      <td align="left" style="padding:5px;">Unspecified</td>
+      <td align="left" style="padding:5px;">Academic Institution <br/> New Faculty/New Investigator</td>
+      <td align="left" style="padding:5px;">Unrestricted</td>
+      <td align="left" style="padding:5px;" 
+          title="The purpose of this Funding Opportunity Announcement (FOA) is to promote research that transforms understanding of HIV transmission, the HIV care continuum, and HIV comorbidities using Big Data Science (BDS). These approaches should include projects to assemble big data sources, conduct robust and reproducible analyses, and create meaningful visualization of big data. 
+
+          Components of Participating Organizations 
+          National Institute of Allergy and Infectious Diseases (NIAID) 
+          National Cancer Institute (NCI) 
+          National Institute on Drug Abuse (NIDA) 
+          National Institute of Mental Health (NIMH) ">Hover to see abstract      
       </td>
-      <td align="left" style="padding:5px;"></td>
+      <td align="left" style="padding:5px;">07 May 2017</td>
     </tr>
 
-    <tr bgcolor="#bdb">
-      <td align="left" style="padding:5px;"><a href=""></a></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;"></td>
-      <td align="left" style="padding:5px;"></td>
+    <tr bgcolor="#bbd">
+      <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/169222">Ford-Mozilla Open Web Fellow Program</a></td>
+      <td align="left" style="padding:5px;"><a href="https://advocacy.mozilla.org/en-US/open-web-fellows/overview">Mozilla</a></td>
+      <td align="left" style="padding:5px;">Upper  $60,000USD <br/> Lower  $60,000USD</td>
+      <td align="left" style="padding:5px;">New Faculty/New Investigator <br/> Ph.D./M.D./Other Professional</td>
+      <td align="left" style="padding:5px;">Unspecified</td>
+      <td align="left" style="padding:5px;" title="The Open Web Fellows program - a collaboration between the Ford Foundation and Mozilla - is a 10-month fellowship program that brings together technology talent and civil society organizations from around the world to advance and protect the open internet. 
+
+      The program aims to: 
+      - Increase public awareness and understanding of internet policy issues 
+      - Support and enable career paths in the internet policy and advocacy sectors 
+      - Celebrate and nurture a vibrant network of internet advocacy organizations 
+
+      Fellows are makers and doers passionate about the open internet. They may bring specialities in data visualization, design, development, storytelling, research or policy analysis. While they may not have previous experience with internet advocacy, policy or activism, they are comfortable performing as technologists, experimenters and curious contributors to the open internet movement.">Hover to see abstract</td>
+      <td align="left" style="padding:5px;">20 Mar 2017 <br/>Anticipated</td>
     </tr>
+
     </table>
 
 
