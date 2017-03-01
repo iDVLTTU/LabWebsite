394d43ad4a118a334535e87f07c4f0e2bb7d53c9
Md. Yasin Kabir <mykabir0@gmail.com>
2017-03-01 14:31:28 -0600

    new news

--- grants.php
+++ grants.php
@@ -28,7 +28,7 @@ include 'menu.php'; ?>
       <td align="left" style="padding:5px;">Academic Institution <br/> New Faculty/New Investigator</td>
       <td align="left" style="padding:5px;">United States</td>
       <td align="left" style="padding:5px;" title="The Advances in Biological Informatics (ABI) program seeks to encourage new approaches to the analysis and dissemination of biological knowledge for the benefit of both the scientific community and the broader public. The ABI program is especially interested in the development of informatics tools and resources that have the potential to advance- or transform- research in biology supported by the Directorate for Biological Sciences at the National Science Foundation.  The ABI program accepts three major types of proposals: Innovation awards that seek to pioneer new approaches to the application of informatics to biological problems, Development awards that seek to provide robust cyberinfrastructure that will enable transformative biological research, and Sustaining awards that seek to support ongoing operations and maintenance of existing cyberinfrastructure that is critical for continued advancement of priority biological research.">Hover to see abstract</td>
-      <td align="left" style="padding:5px;">09 Sep 2017</td>
+      <td align="left" style="padding:5px;">08 Sep 2017</td>
     </tr>
     
     <tr bgcolor="#bdb">
@@ -49,7 +49,18 @@ include 'menu.php'; ?>
       <td align="left" style="padding:5px;">07 May 2017</td>
     </tr>
 
-    <tr bgcolor="#bbd">
+     <tr bgcolor="#bbd">
+      <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/171974">Research Collaborations (APTRC) <br/>for Cancer Research (Admin Supp)</a></td>
+      <td align="left" style="padding:5px;"><a href="https://grants.nih.gov/grants/guide/pa-files/PA-17-143.html">HHS,NIH</a></td>
+      <td align="left" style="padding:5px;">Upper  $50,000USD</td>
+      <td align="left" style="padding:5px;">Academic Institution <br/> New Faculty/New Investigator</td>
+      <td align="left" style="padding:5px;">United States</td>
+      <td align="left" style="padding:5px;" title="The National Cancer Institute (NCI) administers two unique programs focused on supporting technology development research that offer transformative potential for advancing cancer research. The Innovative Molecular Analysis Technologies (IMAT) program supports highly innovative, data-generating platforms and methodologies while the Informatics Technologies for Cancer Research (ITCR) program supports powerful data processing and visualization technologies. 
+      To accelerate the development of new enabling cancer technologies, the NCI invites requests for supplemental funding in fiscal years 2017 and 2018 to support and encourage new multidisciplinary scientific collaborations among awardees from these two programs. The goal of the Activities to Promote Technology Research Collaborations (APTRC) is to support new collaborations that bring together complementary technology platforms and methodologies to enhance the capabilities of either in a way that advances cancer research or clinical care. The proposed APTRC activities must be within the overall scope of active parent grants.">Hover to see abstract</td>
+      <td align="left" style="padding:5px;">12 Apr 2017</td>
+    </tr>
+
+    <tr bgcolor="#bdd">
       <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/169222">Ford-Mozilla Open Web Fellow Program</a></td>
       <td align="left" style="padding:5px;"><a href="https://advocacy.mozilla.org/en-US/open-web-fellows/overview">Mozilla</a></td>
       <td align="left" style="padding:5px;">Upper  $60,000USD <br/> Lower  $60,000USD</td>
