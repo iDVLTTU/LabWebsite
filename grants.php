87f24e4ad0b0d089f019cf603a7ebdc532e43ac1
Md. Yasin Kabir <mykabir0@gmail.com>
2017-03-07 18:45:19 -0600

    new grant

--- grants.php
+++ grants.php
@@ -21,7 +21,9 @@ include 'menu.php'; ?>
       <td> <b>Deadline</b></td>
     </tr>
 
-     <tr bgcolor="#bbd">
+
+
+     <tr bgcolor="#bdb">
       <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/68670">Advances in Biological Informatics</a></td>
       <td align="left" style="padding:5px;"><a href="">NSF</a></td>
       <td align="left" style="padding:5px;">Unspecified</td>
@@ -29,6 +31,18 @@ include 'menu.php'; ?>
       <td align="left" style="padding:5px;">United States</td>
       <td align="left" style="padding:5px;" title="The Advances in Biological Informatics (ABI) program seeks to encourage new approaches to the analysis and dissemination of biological knowledge for the benefit of both the scientific community and the broader public. The ABI program is especially interested in the development of informatics tools and resources that have the potential to advance- or transform- research in biology supported by the Directorate for Biological Sciences at the National Science Foundation.  The ABI program accepts three major types of proposals: Innovation awards that seek to pioneer new approaches to the application of informatics to biological problems, Development awards that seek to provide robust cyberinfrastructure that will enable transformative biological research, and Sustaining awards that seek to support ongoing operations and maintenance of existing cyberinfrastructure that is critical for continued advancement of priority biological research.">Hover to see abstract</td>
       <td align="left" style="padding:5px;">08 Sep 2017</td>
+    </tr>
+
+    <tr bgcolor="#bbd">
+      <td align="left" style="padding:5px;"><a href="https://www.grants.gov/web/grants/view-opportunity.html?oppId=292128">Fiscal Year 2018 Defense University <br/>Research Instrumentation Program (DURIP)</a></td>
+      <td align="left" style="padding:5px;"><a href="http://www.wpafb.af.mil/Welcome/Fact-Sheets/Display/Article/842050">AFOSR</a></td>
+      <td align="left" style="padding:5px;">Upper  $1,500,000  <br/> Lower  $50,000</td>
+      <td align="left" style="padding:5px;">Institutions of higher education</td>
+      <td align="left" style="padding:5px;">United States</td>
+      <td align="left" style="padding:5px;" 
+          title="The Department of Defense (DoD) announces the Fiscal Year 2018 Defense University Research Instrumentation Program (DURIP). DURIP is designed to improve the capabilities of accredited United States (U.S.) institutions of higher education to conduct research and to educate scientists and engineers in areas important to national defense, by providing funds for the acquisition of research equipment or instrumentation. For-profit organizations are not eligible for DURIP funding. We refer to eligible institutions of higher education as universities in the rest of this announcement. DURIP is part of the University Research Initiative (URI).">Hover to see abstract      
+      </td>
+      <td align="left" style="padding:5px;">07 July 2017</td>
     </tr>
     
     <tr bgcolor="#bdb">
@@ -53,7 +67,7 @@ include 'menu.php'; ?>
     <tr bgcolor="#bbd">
       <td align="left" style="padding:5px;"><a href="http://pivot.cos.com/funding_opps/169222">Ford-Mozilla Open Web Fellow Program</a></td>
       <td align="left" style="padding:5px;"><a href="https://advocacy.mozilla.org/en-US/open-web-fellows/overview">Mozilla</a></td>
-      <td align="left" style="padding:5px;">Upper  $60,000USD <br/> Lower  $60,000USD</td>
+      <td align="left" style="padding:5px;">Upper  $60,000<br/> Lower  $60,000</td>
       <td align="left" style="padding:5px;">New Faculty/New Investigator <br/> Ph.D./M.D./Other Professional</td>
       <td align="left" style="padding:5px;">Unspecified</td>
       <td align="left" style="padding:5px;" title="The Open Web Fellows program - a collaboration between the Ford Foundation and Mozilla - is a 10-month fellowship program that brings together technology talent and civil society organizations from around the world to advance and protect the open internet. 
