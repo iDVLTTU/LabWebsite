1a6c7ade51bf1e7b5acd71d355fd5817f84226f4
Tommy Dang <nhontuan@gmail.com>
2017-03-17 12:00:42 -0500

    Update

--- publications.php
+++ publications.php
@@ -7,14 +7,22 @@ include 'menu.php';
   <div id="site_content">
     <div id="top_border"></div>
     <div class="content fullPage publicationPage">
-      
+
+       <h4>IN PREPARATION</h4>
+       <ul>
+            <li>Vinh Nguyen and Tommy Dang. Submitting a paper to IEEE infoVis on March 31.</li><br>
+            <li>Tommy Dang. Submitting a paper to IEEE VAST 2017 on March 31.</li><br>
+            <li>Vinh Nguyen and Tommy Dang. Submitting a paper to GLBIO 2017 on March 13.</li><br>
+            <li>Yasin Kabir, Vinh Nguyen, and Tommy Dang. Submitting a paper to VINCI 2017 on March 15.</li><br>
+        </ul>
       <h4>IN SUBMISSION</h4>
         <ul>
-          <li>Tommy Dang. Submitted to IEEE Transactions on Visualization and Computer Graphics.</li><br>
-          <li>Tommy Dang. Submitted to EuroVis 2017. </li>
+          <!--<li>Tommy Dang and Angus Forbes. Submitted to IEEE Transactions on Visualization and Computer Graphics.</li>-->
+          <li>Tommy Dang and Long Nguyen. Submitted a short paper to EnvirVis 2017 on Feb 17. </li>
+          <li>Vinh Nguyen and Tommy Dang. Submitted a short paper to EuroVis 2017 on Feb 17. </li>
+          <li>Vinh Nguyen and Tommy Dang. Submitted a paper to VISUAL 2017 on Feb 27. </li>
         </ul>
-      
-      
+
           <div class="Border"></div> 
           <h4>PAPERS</h4>
            <ul>
