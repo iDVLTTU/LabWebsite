5f92b3b2c7908cf189fe13cb412ed5da3d0ad567
Md. Yasin Kabir <mykabir0@gmail.com>
2017-04-03 16:04:12 -0500

    adding news

--- newsSidebar.php
+++ newsSidebar.php
@@ -7,13 +7,15 @@
                  <?php
                         $xmls=simplexml_load_file("newsArchive.xml") or die("Error: Cannot create object");
                         $i = 0;
-                        foreach ($xmls as $xml) { 
-                          echo "<h4>" . $xml->headline->asXml() . "</h4>";
+                        foreach ($xmls as $xml) {
+                        if($i!=count($xmls)-1)
+                          echo "<h4>". '<a href="news.php?id=' .$i.'">' . $xml->headline->asXml() . '</a>' . "</h4>";
+                          else
+                          echo "<h4>". $xml->headline->asXml() . "</h4>";
+                        $i++;
                           echo "<h5>" . $xml->date->asXml() . "</h5>";
-                          echo "<p>" . $xml->sortNews->asXml() . "</p>"; 
-                          if($i!=count($xmls)-1)
-                          echo '<a href="news.php?id=' .$i.'">Read More</a>';
-                          $i++; ?>
+                          echo "<p>" . $xml->sortNews->asXml() . "</p>";
+                           ?>
                           <div style="clear:both;"></div>
                           <div class="sidebarNewsBorder"></div> 
                           <?php 
